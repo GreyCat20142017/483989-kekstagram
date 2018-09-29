9e70db8ea9fb9fbcccdc36280b877d3c9ed762cd
GreyCat20142017 <nrz3siaatg81@mail.ru>
2018-09-29 16:29:26 +0300

    Задание 7.1 (25) - Кекстаграм

--- js/common.js
+++ js/common.js
@@ -1,9 +1,31 @@
 'use strict';
 
 (function () {
+  var DEBOUNCE_INTERVAL = 500;
 
   var getRandomFromRange = function (min, max) {
     return Math.floor(min + Math.random() * (max + 1 - min));
+  };
+
+  var shuffleArray = function (arr) {
+    var array = arr.slice();
+    for (var i = array.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+    return array;
+  };
+
+  var getStringCompareResult = function (left, right) {
+    if (left > right) {
+      return 1;
+    } else if (left < right) {
+      return -1;
+    } else {
+      return 0;
+    }
   };
 
   window.common = {
@@ -33,6 +55,40 @@
 
     getRandomAvatar: function () {
       return 'img/avatar-' + getRandomFromRange(1, 6) + '.svg';
+    },
+
+    getRandomLimitedSetFromArray: function (arr, setLength) {
+      var reSortedArray = shuffleArray(arr);
+      reSortedArray.length = Math.min(setLength, reSortedArray.length);
+      return reSortedArray;
+    },
+
+    getPhotosByRank: function (arr) {
+      var reSortedArray = arr.slice().sort(function (firstItem, secondItem) {
+        var rank = (firstItem.comments && secondItem.comments) ? (secondItem.comments.length - firstItem.comments.length) : 0;
+        if (rank === 0) {
+          rank = (firstItem.likes && secondItem.likes) ? (parseInt(secondItem.likes, 10) - parseInt(firstItem.likes, 10)) : 0;
+        }
+        if (rank === 0) {
+          rank = (firstItem.url && secondItem.url) ? getStringCompareResult(firstItem.url, secondItem.url) : 0;
+        }
+        return rank;
+      });
+      return reSortedArray;
+    },
+
+    debounce: function (action) {
+      var lastTimeout = null;
+      return function () {
+        var args = arguments;
+        if (lastTimeout) {
+          window.clearTimeout(lastTimeout);
+        }
+        lastTimeout = window.setTimeout(function () {
+          action.apply(null, args);
+        }, DEBOUNCE_INTERVAL);
+      };
     }
+
   };
 })();
